847a59b450b19bf65bc8239389c13db59c3fd02d
deborah-maitrejean <deborah.maitrejean@gmail.com>
2018-04-19 13:59:08 +0200

    Manager class from MVC last activity course added

---
+++ model/Manager.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace OpenClassrooms\Blog\Model;
+
+class Manager {
+    protected function dbConnect() {
+        $db = new \PDO('mysql:host=localhost;dbname=test;charset=utf8', 'dbuser', '');
+        return $db;
+    }
+}
