b48ff5e7bdeb43eac20cff6ac0a8130ba9db82fa
deborah-maitrejean <deborah.maitrejean@gmail.com>
2018-04-20 16:25:27 +0200

    backend template head, link, nav & footer added

--- view/backend/template.php
+++ view/backend/template.php
@@ -1,1 +1,104 @@
-<?php
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Administration du blog</title>
+    <meta name="description" content="">
+    <meta name="robots" content="noindex, nofollow, noarchive">
+    <meta name="language" content="fr">
+    <meta name="copyright" content="Jean Forteroche">
+    <meta charset="utf-8">
+
+    <!-- TinyMCE editor -->
+    <link rel="stylesheet" type="text/css" href="">
+
+    <!-- Add icon library -->
+    <link href="vendor/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+
+    <link rel="stylesheet" href="public/css/style.css" type="text/css">
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css">
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+    <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+    <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+
+    <!-- Bootstrap core JavaScript -->
+    <script type="text/javascript" src="vendor/jquery/jquery.min.js"></script>
+</head>
+
+<body>
+
+<!-- Static navbar -->
+<nav class="navbar navbar-default">
+    <div class="container-fluid">
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand" href="index.php">Billet simple pour l'Alaska</a>
+        </div>
+        <div id="navbar" class="navbar-collapse collapse">
+            <ul class="nav navbar-nav">
+                <li class="active"><a href="#">Accueil de l'administration du site</a></li>
+                <li><a href="">Poster / modifier un billet</a></li>
+                <li><a href="">Modérer les commentaires</a></li>
+            </ul>
+            <ul class="nav navbar-nav navbar-right">
+                <li class=""><a href=""><span class="fa fa-time"></span> Déconnexion <span class="sr-only">(current)</span></a></li>
+            </ul>
+        </div><!--/.nav-collapse -->
+    </div><!--/.container-fluid -->
+</nav>
+
+<div class="container">
+   <!-- <?= $content ?> -->
+</div>
+
+<div class="container-fluid">
+    <div class="row">
+        <div class="col-lg-8 col-md-10 mx-auto" id="footer">
+            <footer>
+                <ul class="list-inline text-center">
+                    <li class="list-inline-item">
+                        <a href="https://twitter.com/D_Maitrejean" target="_blank">
+                            <span class="fa-stack fa-lg">
+                                <i class="fa fa-circle fa-stack-2x"></i>
+                                <i class="fa fa-twitter fa-stack-1x fa-inverse"></i>
+                            </span>
+                        </a>
+                    </li>
+                    <li class="list-inline-item">
+                        <a href="https://www.linkedin.com/in/d%C3%A9borah-maitrejean" target="_blank">
+                            <span class="fa-stack fa-lg">
+                                <i class="fa fa-circle fa-stack-2x"></i>
+                                <i class="fa fa-linkedin fa-stack-1x fa-inverse"></i>
+                            </span>
+                        </a>
+                    </li>
+                    <li class="list-inline-item">
+                        <a href="https://github.com/deborah-maitrejean" target="_blank">
+                            <span class="fa-stack fa-lg">
+                                <i class="fa fa-circle fa-stack-2x"></i>
+                                <i class="fa fa-github fa-stack-1x fa-inverse"></i>
+                            </span>
+                        </a>
+                    </li>
+                </ul>
+                <p class="copyright text-muted">2018 Copyright &copy; Jean Forteroche</p>
+            </footer>
+        </div>
+    </div>
+</div>
+
+<!-- Latest compiled and minified JavaScript -->
+<script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.1/js/bootstrap.min.js"></script>
+<script src="public/js/cookies.js"></script>
+
+</body>
+</html>
