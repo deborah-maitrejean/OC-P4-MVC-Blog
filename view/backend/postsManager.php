2e8211a458b9f067886c22041079d35add34dee7
deborah-maitrejean <deborah.maitrejean@gmail.com>
2018-04-21 17:46:50 +0200

    postsManager file created

---
+++ view/backend/postsManager.php
@@ -0,0 +1,12 @@
+<?php $title = 'Billet simple pour l\'Alaska'; ?>
+
+<?php ob_start(); ?>
+
+<div class="row">
+    <div class="col-lg-12">
+
+    </div>
+</div>
+
+<?php $content = ob_get_clean(); ?>
+<?php require('template.php'); ?>
