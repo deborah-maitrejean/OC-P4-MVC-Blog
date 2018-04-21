9b55a5b7f40eb12cc71a0a999626914a72bb0933
deborah-maitrejean <deborah.maitrejean@gmail.com>
2018-04-21 18:02:42 +0200

    postsManager view update

--- view/backend/postsManager.php
+++ view/backend/postsManager.php
@@ -4,7 +4,35 @@
 
 <div class="row">
     <div class="col-lg-12">
+        <table class="table table-bordered">
+            <caption></caption>
 
+            <thead>
+            <tr class="info">
+                <th>Auteur</th>
+                <th>Contenu</th>
+                <th>Date de publication</th>
+                <th>Actions</th>
+            </tr>
+            </thead>
+
+            <tbody>
+            <?php
+            while ($post = $posts->fetch()) {
+                ?>
+                <tr>
+                    <td class="default"><?= htmlspecialchars($post['author']) ?></td>
+                    <td class="default"><?= nl2br(htmlspecialchars($post['content'])) ?></td>
+                    <td class="default"><?= $post['creation_date_fr'] ?></td>
+                    <td class="danger"><a href="">Modifier</a> <a href="">Supprimer</a></td>
+                </tr>
+                <?php
+            }
+            $posts->closeCursor();
+            ?>
+            </tbody>
+
+        </table>
     </div>
 </div>
 
