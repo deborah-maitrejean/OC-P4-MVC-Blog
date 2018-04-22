6190b2538c010b41f2c9c840346ff7a476510cf7
deborah-maitrejean <deborah.maitrejean@gmail.com>
2018-04-22 15:45:11 +0200

    Plus anciens button style updated

--- view/frontend/ListPostsView.php
+++ view/frontend/ListPostsView.php
@@ -57,8 +57,8 @@ $posts->closeCursor();
 ?>
     <!-- Pager -->
     <div class="row" id="older-posts-btn-section">
-        <div class="col-lg-offset-10 col-lg-2">
-            <a class="btn btn-primary" href="#">Plus anciens &rarr;</a>
+        <div class="col-lg-offset-5 col-lg-2">
+            <a class="btn btn-default" href="#">Plus anciens &rarr;</a>
         </div>
     </div>
 
