5891af7cfe0f7098155d8e6b40df586bed8ac2ba
deborah-maitrejean <deborah.maitrejean@gmail.com>
2018-04-23 15:22:09 +0200

    fontawesome class updated on newPostView

--- view/backend/newPostView.php
+++ view/backend/newPostView.php
@@ -1,7 +1,26 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: Utilisateur
- * Date: 23/04/2018
- * Time: 14:59
- */
+<?php $title = 'Administration du site - Nouveau billet' ?>
+
+<?php ob_start(); ?>
+
+<div class="row">
+    <div class="col-lg-12">
+        <h2><i class="fa fa-pencil-square-o"></i> Rédiger un billet</h2>
+        <form action="index.php?action=publishPost" method="post">
+            <div class="form-group">
+                <label for="title">Titre:</label>
+                <input type="text">
+            </div>
+            <div class="form-group">
+                <label for="content">Contenu:</label>
+                <textarea id="content" name="content" class="form-control"></textarea>
+            </div>
+            <div class="form-group">
+                <input type="submit" value="Publier" class="btn btn-success btn-lg">
+                <input type="reset" class="btn btn-danger btn-lg">
+            </div>
+        </form>
+    </div>
+</div>
+
+<?php $content = ob_get_clean(); ?>
+<?php require('template.php'); ?>
