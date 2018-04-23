52b36813102856dbee5276113d7d68350d80b199
deborah-maitrejean <deborah.maitrejean@gmail.com>
2018-04-23 18:32:58 +0200

    postsManager updated

--- view/backend/postsManager.php
+++ view/backend/postsManager.php
@@ -25,7 +25,7 @@
                         <td class="success" scope="row"><?= $post['title']; ?></td>
                         <td class="default" scope="row"><?= strip_tags($post['postExcerpt']); ?>...</td>
                         <td class="warning" scope="row"><?= $post['creation_date_fr']; ?></td>
-                        <td class="danger" scope="row"><a href="" class="btn btn-success">Voir ou Modifier</a></td>
+                        <td class="danger" scope="row"><a href="index.php?action=viewOrChangePost&amp;postId=<?= $post['id']; ?>" class="btn btn-success">Voir ou Modifier</a></td>
                         <td class="danger" scope="row"><a href="" class="btn btn-danger">Supprimer</a></td>
                     </tr>
                     <?php
