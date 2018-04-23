a5d7967b1a6d2a2f08e03eadc8a27589b98396b4
deborah-maitrejean <deborah.maitrejean@gmail.com>
2018-04-23 18:12:46 +0200

    strip_tags function added on postsMnager form td

--- view/backend/postsManager.php
+++ view/backend/postsManager.php
@@ -22,9 +22,9 @@
                 while ($post = $posts->fetch()) {
                     ?>
                     <tr>
-                        <td class="success" scope="row"><?= htmlspecialchars($post['title']) ?></td>
-                        <td class="default" scope="row"><?= nl2br(htmlspecialchars($post['postExcerpt'])) ?>...</td>
-                        <td class="warning" scope="row"><?= $post['creation_date_fr'] ?></td>
+                        <td class="success" scope="row"><?= $post['title']; ?></td>
+                        <td class="default" scope="row"><?= strip_tags($post['postExcerpt']); ?>...</td>
+                        <td class="warning" scope="row"><?= $post['creation_date_fr']; ?></td>
                         <td class="danger" scope="row"><a href="" class="btn btn-success">Voir ou Modifier</a></td>
                         <td class="danger" scope="row"><a href="" class="btn btn-danger">Supprimer</a></td>
                     </tr>
