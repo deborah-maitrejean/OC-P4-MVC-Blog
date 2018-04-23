bfd3a750ca588e7fb6bc7777ef268c7fd713b3fd
deborah-maitrejean <deborah.maitrejean@gmail.com>
2018-04-23 18:41:50 +0200

    postView page created

--- view/backend/postView.php
+++ view/backend/postView.php
@@ -1,7 +1,32 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: Utilisateur
- * Date: 23/04/2018
- * Time: 18:20
- */
+<?php $title = 'Administration du site - Modifier un billet' ?>
+
+<?php ob_start(); ?>
+
+<div class="row tinymce-post-row">
+    <div class="col-lg-12">
+        <h2><i class="fa fa-pencil-square-o"></i> Rédiger un billet</h2>
+        <form action="index.php?action=publishPost" method="post">
+            <div class="form-group">
+                <label for="title">Titre:</label>
+                <input type="text" name="title">
+            </div>
+            <div class="form-group">
+                <label for="content">Contenu:</label>
+                <textarea id="content" name="content" class="tinymce"></textarea>
+            </div>
+            <div class="form-group">
+                <input type="submit" name="submit" value="Publier" class="btn btn-success btn-lg">
+                <input type="hidden" name="author" value="Jean Forteroche">
+                <input type="reset" class="btn btn-danger btn-lg">
+            </div>
+        </form>
+    </div>
+</div>
+
+<!-- tinymce editor scripts -->
+<script src="public/js/tinymce/jquery.tinymce.min.js"></script>
+<script src="public/js/tinymce/tinymce.min.js"></script>
+<script src="public/js/tinymce/init-tinymce.js"></script>
+
+<?php $content = ob_get_clean(); ?>
+<?php require('template.php'); ?>
