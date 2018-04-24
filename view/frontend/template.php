f1bf3b82b5d5360116a15964248066dc04ec02d2
deborah-maitrejean <deborah.maitrejean@gmail.com>
2018-04-24 13:40:25 +0200

    condition added on li navbar to change class when tab is active in frontend template

--- view/frontend/template.php
+++ view/frontend/template.php
@@ -48,10 +48,10 @@
         </div>
         <div id="navbar" class="navbar-collapse collapse">
             <ul class="nav navbar-nav">
-                <li class="active"><a href="#">Accueil</a></li>
-                <li><a href="index.php?action=allPostsView">Tous les billets</a></li>
-                <li><a href="index.php?action=about">A propos</a></li>
-                <li><a href="index.php?action=contact">Contact</a></li>
+                <li <?php if (stripos($_SERVER['REQUEST_URI'],'index.php') !== false) {echo 'class="active"';} ?>><a href="index.php">Accueil</a></li>
+                <li <?php if (stripos($_SERVER['REQUEST_URI'],'index.php?action=allPostsView') !== false) {echo 'class="active"';} ?>><a href="index.php?action=allPostsView">Tous les billets</a></li>
+                <li <?php if (stripos($_SERVER['REQUEST_URI'],'index.php?action=about') !== false) {echo 'class="active"';} ?>><a href="index.php?action=about">A propos</a></li>
+                <li <?php if (stripos($_SERVER['REQUEST_URI'],'index.php?action=contact') !== false) {echo 'class="active"';} ?>><a href="index.php?action=contact">Contact</a></li>
             </ul>
             <ul class="nav navbar-nav navbar-right">
                 <li class=""><a href="index.php?action=adminConnexion"><span class="fa fa-user"></span> Admin <span class="sr-only">(current)</span></a></li>
