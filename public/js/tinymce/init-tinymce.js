d0e23f7b573c56e6a8cdc098b76d2cd017a7baad
deborah-maitrejean <deborah.maitrejean@gmail.com>
2018-04-24 10:41:55 +0200

    improve tinymce view

--- public/js/tinymce/init-tinymce.js
+++ public/js/tinymce/init-tinymce.js
@@ -14,6 +14,13 @@ tinymce.init({
     /* display statusbar */
     statubar: true,
 
+    /* plugin */
+    plugins: [
+        "advlist autolink link image lists charmap print preview hr anchor pagebreak",
+        "searchreplace wordcount visualblocks visualchars code fullscreen insertdatetime media nonbreaking",
+        "save table contextmenu directionality emoticons template paste textcolor"
+    ],
+
     /* toolbar */
     toolbar: "insertfile undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image | print preview media fullpage | forecolor backcolor emoticons",
 
