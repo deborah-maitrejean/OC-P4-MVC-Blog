0a5e9aeb0aa5e715e0e5f99d20b0488bc0d60b35
deborah-maitrejean <deborah.maitrejean@gmail.com>
2018-04-25 18:15:21 +0200

    report link updated in postView

--- view/frontend/postView.php
+++ view/frontend/postView.php
@@ -46,7 +46,7 @@
                     <strong><?= htmlspecialchars($comment['author']) ?></strong> le <?= $comment['creation_date_fr'] ?>
                     <?php
                     if ($comment['reported'] != 1): ?>
-                    <a href="index.php?action=reportComment&amp;commentId=<?= $comment['id'] ?>&amp;postId=<?= $post['id'] ?>">(signaler)</a>
+                    <a href="index.php?action=reportComment&amp;commentId=<?= $comment['id'] ?>&amp;reported=1&amp;postId=<?= $post['id'] ?>">(signaler)</a>
                     <?php endif; ?>
                 </p>
 
