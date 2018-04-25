4cf8042c5a16364a2ef749af432c1fcf62e215e1
deborah-maitrejean <deborah.maitrejean@gmail.com>
2018-04-25 13:38:40 +0200

    Router attributes added

--- classes/Router.php
+++ classes/Router.php
@@ -6,6 +6,10 @@
 
 include_once('controller/Frontend.php');
 include_once('controller/Backend.php');
-class Router {
 
+class Router {
+    private $request;
+    private $routes = [ // tableau qui récupère toutes les routes, il fait un lien entre ce qu'on demande, et un autre tbealu qui intègre le controller
+
+    ];
 }
