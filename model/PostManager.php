dc94ce1d77cf813e93c7e4cc8cdce4defafedbbb
deborah-maitrejean <deborah.maitrejean@gmail.com>
2018-04-25 18:40:01 +0200

    updatePost method updated on PostManaer model

--- model/PostManager.php
+++ model/PostManager.php
@@ -50,7 +50,7 @@ class PostManager extends Manager {
     }
     public function updatePost($title, $content, $postId){
         $db = $this->dbConnect();
-        $req = $db->prepare('UPDATE posts SET title = ?, content = ?, creation_date = NOW() WHERE id = ?');
+        $req = $db->prepare('UPDATE posts SET title = ?, content = ? WHERE id = ?');
         $affectedPost = $req->execute(array($title, $content, $postId));
 
         return $affectedPost;
