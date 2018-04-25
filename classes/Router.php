6e8d478be22262644bb16abc03f76d485d3bc068
deborah-maitrejean <deborah.maitrejean@gmail.com>
2018-04-25 13:47:39 +0200

    routes added on Router class

--- classes/Router.php
+++ classes/Router.php
@@ -10,6 +10,13 @@ include_once('controller/Backend.php');
 class Router {
     private $request;
     private $routes = [ // tableau qui récupère toutes les routes, il fait un lien entre ce qu'on demande, et un autre tbealu qui intègre le controller
-
+        '' => ['controller'=>'Frontend', 'method'=>'listPostsExcerpt'],
+        'allPostsView' => ['controller'=>'Frontend', 'method'=>'listPosts'], // appelle un controller qui s'appelle Backend
+        'about' => ['controller'=>'Frontend', 'method'=>'aboutView'],
+        'contact' => ['controller'=>'Frontend', 'method'=>'contactView'],
+        'adminConnexion' => ['controller'=>'Frontend', 'method'=>'adminView'],
+        'addComment' => ['controller'=>'Frontend', 'method'=>'addComment'],
+        'reportComment' => ['controller'=>'Frontend', 'method'=>'reportComment']
+        //editComment
     ];
 }
