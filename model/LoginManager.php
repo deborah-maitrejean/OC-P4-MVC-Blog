c4433a81f4dcdcc3c9474ba65c4c48610ba53670
deborah-maitrejean <deborah.maitrejean@gmail.com>
2018-04-26 21:26:33 +0200

    getLgin method updated in LoginManager model

--- model/LoginManager.php
+++ model/LoginManager.php
@@ -1,20 +1,14 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: Utilisateur
- * Date: 21/04/2018
- * Time: 14:15
- */
 
 namespace Blog\Model;
 
 require_once("model/Manager.php");
 
 class LoginManager extends Manager {
-    public function getLogin($email, $password){
+    public function getLogin($email, $passHach){
         $db = $this->dbconnect();
         $req = $db->prepare('SELECT * FROM logins WHERE email = ? AND password = ?');
-        $req->execute(array($email, $password));
+        $req->execute(array($email, $passHach));
         $req = $req->fetch();
 
         return $req;
