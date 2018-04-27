b643f67ae86fc29b2adaaded04eb2b4bded2461a
deborah-maitrejean <deborah.maitrejean@gmail.com>
2018-04-27 20:43:28 +0200

    create Comments entity

--- entity/Comments.php
+++ entity/Comments.php
@@ -1,12 +1,109 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: Utilisateur
- * Date: 27/04/2018
- * Time: 20:28
- */
+namespace Entity;
 
 class Comments
 {
+    private $id;
+    private $author;
+    private $content;
+    private $creation_date;
+    private $post_id;
+    private $post_title;
+
+    /**
+     * @return mixed
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param mixed $id
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getAuthor()
+    {
+        return $this->author;
+    }
+
+    /**
+     * @param mixed $author
+     */
+    public function setAuthor($author)
+    {
+        $this->author = $author;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getContent()
+    {
+        return $this->content;
+    }
+
+    /**
+     * @param mixed $content
+     */
+    public function setContent($content)
+    {
+        $this->content = $content;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getCreationDate()
+    {
+        return $this->creation_date;
+    }
+
+    /**
+     * @param mixed $creation_date
+     */
+    public function setCreationDate($creation_date)
+    {
+        $this->creation_date = $creation_date;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getPostId()
+    {
+        return $this->post_id;
+    }
+
+    /**
+     * @param mixed $post_id
+     */
+    public function setPostId($post_id)
+    {
+        $this->post_id = $post_id;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getPostTitle()
+    {
+        return $this->post_title;
+    }
+
+    /**
+     * @param mixed $post_title
+     */
+    public function setPostTitle($post_title)
+    {
+        $this->post_title = $post_title;
+    }
 
 }
