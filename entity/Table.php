02b7fdef453158f71a6a9558ba3ebf9cbce8a5bb
deborah-maitrejean <deborah.maitrejean@gmail.com>
2018-04-27 21:47:37 +0200

    create Table entity width hydrate function

---
+++ entity/Table.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Utilisateur
+ * Date: 27/04/2018
+ * Time: 21:45
+ */
+
+namespace Entity;
+
+
+class Table {
+    public function hydrate(array $data)
+    {
+        foreach ($data as $key => $value){
+            $setter = 'set' . ucfirst($key);
+            $this->$setter($value);
+        }
+    }
+}
