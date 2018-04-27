b03df2417f0155c4ce12aae278d4b76f0f4d0ef3
deborah-maitrejean <deborah.maitrejean@gmail.com>
2018-04-27 20:42:26 +0200

    create Posts entity, add method hydrate

--- entity/Posts.php
+++ entity/Posts.php
@@ -1,12 +1,99 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: Utilisateur
- * Date: 27/04/2018
- * Time: 20:22
- */
+namespace Entity;
 
-class Posts
-{
+class Posts {
+    private $id;
+    private $author;
+    private $title;
+    private $content;
+    private $creation_date;
+
+    public function hydrate(array $data)
+    {
+        foreach ($data as $key => $value){
+            $setter = 'set' . ucfirst($key);
+            $this->$setter($value);
+        }
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param mixed $id
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getAuthor()
+    {
+        return $this->author;
+    }
+
+    /**
+     * @param mixed $author
+     */
+    public function setAuthor($author)
+    {
+        $this->author = $author;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getTitle()
+    {
+        return $this->title;
+    }
+
+    /**
+     * @param mixed $title
+     */
+    public function setTitle($title)
+    {
+        $this->title = $title;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getContent()
+    {
+        return $this->content;
+    }
+
+    /**
+     * @param mixed $content
+     */
+    public function setContent($content)
+    {
+        $this->content = $content;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getCreationDate()
+    {
+        return $this->creation_date;
+    }
+
+    /**
+     * @param mixed $creation_date
+     */
+    public function setCreationDate($creation_date)
+    {
+        $this->creation_date = $creation_date;
+    }
 
 }
