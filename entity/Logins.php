b8a2a4f4d4f7ec55f21af80e98b47999cf144e4e
deborah-maitrejean <deborah.maitrejean@gmail.com>
2018-04-27 20:43:03 +0200

    create Logins entity

--- entity/Logins.php
+++ entity/Logins.php
@@ -1,12 +1,58 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: Utilisateur
- * Date: 27/04/2018
- * Time: 20:26
- */
+namespace Entity;
 
 class Logins
 {
+    private $id;
+    private $email;
+    private $password;
+
+    /**
+     * @return mixed
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param mixed $id
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getEmail()
+    {
+        return $this->email;
+    }
+
+    /**
+     * @param mixed $email
+     */
+    public function setEmail($email)
+    {
+        $this->email = $email;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getPassword()
+    {
+        return $this->password;
+    }
+
+    /**
+     * @param mixed $password
+     */
+    public function setPassword($password)
+    {
+        $this->password = $password;
+    }
 
 }
