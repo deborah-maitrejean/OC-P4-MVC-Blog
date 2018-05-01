bea0ecf4c6d184af3a113619d0635623c34b92ef
deborah-maitrejean <deborah.maitrejean@gmail.com>
2018-05-01 20:31:36 +0200

    add pagination div in commentsModeration view

--- view/backend/commentsModeration.php
+++ view/backend/commentsModeration.php
@@ -24,7 +24,8 @@
                 </thead>
 
                 <tbody>
-                <?php foreach ($comments as $comment): ?>
+                <?php if (isset($comments) && $comments != false):
+                    foreach ($comments as $comment): ?>
                     <tr>
                         <td class="success" scope="row"><?= htmlspecialchars($comment->getAuthor()); ?></td>
                         <td class="default" scope="row"><?= nl2br(htmlspecialchars($comment->getContent())); ?></td>
@@ -38,11 +39,39 @@
                             <td class="danger" scope="row"><a href="index.php?action=deleteComment&amp;commentId=<?= $comment->getId(); ?>" class="btn btn-danger">Supprimer</a></td>
                     </tr>
                     <?php endforeach; ?>
+                <?php endif; ?>
                 </tbody>
             </table>
         </div>
     </div>
 </div>
+
+<?php if (isset($currentPage)): ?>
+    <div class="row">
+        <div class="col-lg-12">
+            <ul class="pagination text-center">
+                <?php if ($currentPage - 1 == 0): ?>
+                    <li class="page-item disabled"><span><i class="fa fa-angle-left"></i></span></li>
+                <?php else : ?>
+                    <li class="page-item"><a href="index.php?action=commentsModeration&amp;page=<?=$currentPage - 1 ?>" class="page-link"><i class="fa fa-angle-left"></i></a></li>
+                <?php endif; ?>
+                <?php
+                for ($i = 1; $i <= $nbPages; $i++) {
+                    if ($i == $currentPage): ?>
+                        <li class="page-item active"><a class="page-link"><?= $i ?></a></li>
+                    <?php else : ?>
+                        <li class="page-item"><a href="index.php?action=commentsModeratio&amp;page=<?= $i ?>" class="page-link"><?=$i?></a></li>
+                    <?php endif;
+                } ?>
+                <?php if ($currentPage + 1 > $nbPages): ?>
+                    <li class="page-item disabled"><span><i class="fa fa-angle-right"></i></span></li>
+                <?php else : ?>
+                    <li class="page-item"><a href="index.php?action=commentsModeration&amp;page=<?=$currentPage + 1 ?>" class="page-link"><i class="fa fa-angle-right"></i></a></li>
+                <?php endif; ?>
+            </ul>
+        </div>
+    </div>
+<?php endif; ?>
 
 <?php $content = ob_get_clean(); ?>
 <?php require('template.php'); ?>
