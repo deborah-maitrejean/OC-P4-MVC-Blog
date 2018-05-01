394250f253a252fb677c898a4e047226c2b7de42
deborah-maitrejean <deborah.maitrejean@gmail.com>
2018-05-01 20:03:01 +0200

    update sitemap and data base

--- blogjeanforteroche.sql
+++ blogjeanforteroche.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1
--- Généré le :  lun. 30 avr. 2018 à 23:48
+-- Généré le :  mar. 01 mai 2018 à 20:02
 -- Version du serveur :  10.1.26-MariaDB
 -- Version de PHP :  7.1.8
 
@@ -59,7 +59,46 @@ INSERT INTO `comments` (`id`, `author`, `content`, `creationDate`, `postId`, `po
 (44, 'Mathilde', 'Vraiment passionnant, ça donne envie de prendre son billet ;-)', '2018-04-26 15:17:21', 5, 'Cinquième billet', 0),
 (45, 'Jean-Louis', 'Pas terrible.............', '2018-04-22 19:05:49', 5, 'Cinquième billet', 1),
 (46, 'Emma', 'Mauvais ! J\'ai rarement vu autant de familiarités dans un texte.', '2018-04-24 10:08:19', 3, 'Troisième billet', 1),
-(47, 'Marjorie', 'Bof!', '2018-04-28 21:27:15', 6, 'Sixième billet', 1);
+(47, 'Marjorie', 'Bof!', '2018-04-28 21:27:15', 6, 'Sixième billet', 1),
+(48, 'Henry', 'Extra, j\'aime beaucoup le style ', '2018-03-02 08:24:36', 1, 'Premier billet', 0),
+(49, 'Ryan', 'Super beau', '2018-04-25 17:27:41', 9, 'Dixième billet', 0),
+(50, 'Aicha', 'C\'est d\'une nullité affligeante', '2018-04-10 09:54:36', 1, 'Premier billet', 1),
+(51, 'Salomé', 'Je viens de m\'abonner ;-)', '2018-05-01 18:24:16', 2, 'Second billet', 0),
+(52, 'Maxime', 'c\'est chaud.. enfin, froid :D', '2018-04-25 20:42:55', 3, 'Troisième billet', 0),
+(53, 'Claude', 'Surprenant', '2018-04-26 15:08:19', 3, 'Troisième billet', 0),
+(54, 'Manu', 'Je l\'ai relu deux fois !', '2018-03-23 10:08:19', 3, 'Troisième billet', 0),
+(55, 'Mathis', 'Je découvre par hasard votre blog', '2018-05-01 18:29:21', 4, 'Quatrième billet', 0),
+(56, 'Tom', 'Preum\'s !!', '2018-04-09 19:05:49', 5, 'Cinquième billet', 0),
+(57, 'Thierry', 'cool', '2018-04-10 14:17:21', 5, 'Cinquième billet', 0),
+(58, 'Jean-Louis', 'Toujours un plaisir à lire', '2018-04-15 11:08:01', 5, 'Cinquième billet', 0),
+(59, 'Jean-Louis', 'Super, comme d\'hab', '2018-05-01 18:34:01', 6, 'Sixième billet', 0),
+(60, 'Aicha', 'Intriguant...', '2018-04-10 11:28:45', 6, 'Sixième billet', 0),
+(61, 'Hervé', ':-)', '2018-04-15 20:20:10', 6, 'Sixième billet', 0),
+(62, 'Le montagnard', 'Que de souvenirs... moi aussi j\'y ai séjourné.', '2018-04-17 22:22:19', 6, 'Sixième billet', 0),
+(63, 'Marie', 'Preum\'s !!!!!!!', '2018-04-11 23:42:01', 7, 'Septième billet', 0),
+(64, 'Paul', 'Très beau récit', '2018-04-13 13:41:04', 7, 'Septième billet', 0),
+(65, 'Joris', 'Pas mal', '2018-04-14 14:59:04', 7, 'Septième billet', 0),
+(66, 'Boris', 'Dingue', '2018-04-17 12:00:00', 7, 'Septième billet', 0),
+(67, 'Sophie', 'Preum\'s', '2018-04-15 00:03:37', 8, 'Huitième billet', 0),
+(68, 'Marie', 'Toujours aussi bien écrit', '2018-04-16 01:03:37', 8, 'Huitième billet', 0),
+(69, 'Tom', 'Très beau', '2018-04-17 11:07:37', 8, 'Huitième billet', 0),
+(70, 'Fred', 'De plus en plus mauvais', '2018-04-18 19:59:37', 8, 'Huitième billet', 1),
+(71, 'Momo', 'Extra', '2018-04-19 21:59:59', 8, 'Huitième billet', 0),
+(72, 'Hervé', 'Vraiment incroyable', '2018-04-26 14:19:41', 9, 'Dixième billet', 0),
+(73, 'Hervé', 'Passionnant !', '2018-04-27 16:12:21', 9, 'Dixième billet', 0),
+(74, 'Pat\'', 'Votre style est si vivant ! ', '2018-04-28 19:12:17', 9, 'Dixième billet', 0),
+(75, 'Danny', 'bof bof bof', '2018-04-29 09:11:12', 9, 'Dixième billet', 0),
+(76, 'Sophie', 'Excellent', '2018-04-29 19:25:43', 10, 'Onzième billet', 0),
+(77, 'Mathilde', 'Preum\'s yeah', '2018-04-18 19:27:01', 12, 'Douzième billet', 0),
+(78, 'Tom', 'Très sympa', '2018-04-29 19:29:14', 11, 'Neuvième billet', 0),
+(79, 'Fabrice', 'Bien', '2018-04-18 15:12:14', 11, 'Neuvième billet', 0),
+(80, 'Fab', 'J\'ai adoré !!', '2018-04-30 12:21:15', 10, 'Onzième billet', 0),
+(81, 'Bernard', 'Pas extra hein', '2018-05-01 19:32:11', 10, 'Onzième billet', 0),
+(82, 'Barbara', 'Que de poésie !', '2018-05-01 19:36:03', 10, 'Onzième billet', 0),
+(83, 'Jean', 'xD', '2018-04-19 11:11:01', 12, 'Douzième billet', 0),
+(84, 'Pierre', 'Bien écrit et très drôle', '2018-04-20 12:12:02', 12, 'Douzième billet', 0),
+(85, 'Noémie', 'Et bien moi je trouve ça très nul', '2018-04-22 14:13:07', 12, 'Douzième billet', 0),
+(86, 'Alain', 'Amusant', '2018-05-01 19:40:44', 12, 'Douzième billet', 0);
 
 -- --------------------------------------------------------
 
@@ -143,7 +182,7 @@ ALTER TABLE `posts`
 -- AUTO_INCREMENT pour la table `comments`
 --
 ALTER TABLE `comments`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=48;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=87;
 --
 -- AUTO_INCREMENT pour la table `logins`
 --
