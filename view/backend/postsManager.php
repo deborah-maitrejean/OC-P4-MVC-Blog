4c9d6bc611ab657f08ee62be0fc2d2fb34ebb74e
deborah-maitrejean <deborah.maitrejean@gmail.com>
2018-05-02 02:29:11 +0200

    update order link, add condition, add pagination div, in postsManager view

--- view/backend/postsManager.php
+++ view/backend/postsManager.php
@@ -6,8 +6,8 @@
     <div class="col-lg-12">
         <h2><i class="fa fa-bars"></i> Liste des billets</h2>
         <div>
-            <a href="index.php?action=postsManagerByDate" class="btn btn-default">Plus récent au plus ancien</a>
-            <a href="index.php?action=postsManager" class="btn btn-default">Plus ancien au plus récent</a>
+            <a href="index.php?action=postsManager" class="btn btn-default">Plus récent au plus ancien</a>
+            <a href="index.php?action=postsManager&amp;orderBy=date" class="btn btn-default">Plus ancien au plus récent</a>
         </div>
         <div class="table-responsive">
             <table class="table table-hover table-bordered">
@@ -22,7 +22,8 @@
                 </thead>
 
                 <tbody>
-                <?php foreach ($posts as $post): ?>
+                <?php if (isset($posts) && $posts != false):
+                foreach ($posts as $post): ?>
                     <tr>
                         <td class="success" scope="row"><?= $post->getTitle(); ?></td>
                         <td class="default" scope="row"><?= strip_tags($post->getPostExcerpt()); ?>...</td>
@@ -31,11 +32,39 @@
                         <td class="danger" scope="row"><a href="index.php?action=deletePost&amp;postId=<?= $post->getId(); ?>" class="btn btn-danger">Supprimer</a></td>
                     </tr>
                 <?php endforeach; ?>
+                <?php endif; ?>
                 </tbody>
             </table>
         </div>
     </div>
 </div>
+
+<?php if (isset($currentPage)): ?>
+    <div class="row">
+        <div class="col-lg-12">
+            <ul class="pagination text-center">
+                <?php if ($currentPage - 1 == 0): ?>
+                    <li class="page-item disabled"><span><i class="fa fa-angle-left"></i></span></li>
+                <?php else : ?>
+                    <li class="page-item"><a href="index.php?action=postsManager&amp;page=<?= $currentPage - 1 ?>" class="page-link"><i class="fa fa-angle-left"></i></a></li>
+                <?php endif; ?>
+                <?php
+                for ($i = 1; $i <= $nbPages; $i++) {
+                    if ($i == $currentPage): ?>
+                        <li class="page-item active"><a class="page-link"><?= $i ?></a></li>
+                    <?php else : ?>
+                        <li class="page-item"><a href="index.php?action=postsManager&amp;page=<?= $i ?>" class="page-link"><?=$i?></a></li>
+                    <?php endif;
+                } ?>
+                <?php if ($currentPage + 1 > $nbPages): ?>
+                    <li class="page-item disabled"><span><i class="fa fa-angle-right"></i></span></li>
+                <?php else : ?>
+                    <li class="page-item"><a href="index.php?action=postsManager&amp;page=<?= $currentPage + 1 ?>" class="page-link"><i class="fa fa-angle-right"></i></a></li>
+                <?php endif; ?>
+            </ul>
+        </div>
+    </div>
+<?php endif; ?>
 
 <?php $content = ob_get_clean(); ?>
 <?php require('template.php'); ?>
