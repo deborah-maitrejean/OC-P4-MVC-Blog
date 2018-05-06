c7e8b79698ce2b0b32600ce873461e286d50f2c8
deborah-maitrejean <deborah.maitrejean@gmail.com>
2018-05-06 10:57:03 +0200

    put MyAutoloader into new autoloader directory, update autoload function, add namespace

--- autoloader/MyAutoload.php
+++ autoloader/MyAutoload.php
@@ -1,11 +1,13 @@
 <?php
+namespace Autoloader;
 
 class MyAutoload{
     public static function start(){
         spl_autoload_register(array(__CLASS__, 'autoload'));
     }
     public static function autoload($class){
+        //var_dump($class);die;
         $path = str_replace('\\', '/', $class);
-        require $path.'.php';
+        require '../'.$path.'.php';
     }
 }
