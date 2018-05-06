6c26d7ff36ca9990431df5dc8db42f5b0477bcc6
deborah-maitrejean <deborah.maitrejean@gmail.com>
2018-05-06 14:26:04 +0200

    add phpmailer use and require in ContactManager

--- model/ContactManager.php
+++ model/ContactManager.php
@@ -2,7 +2,12 @@
 
 
 namespace model;
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
 
+require '../vendor/phpmailer/phpmailer/src/Exception.php';
+require '../vendor/phpmailer/phpmailer/src/PHPMailer.php';
+require '../vendor/phpmailer/phpmailer/src/SMTP.php';
 
 class ContactManager {
     public function sendMail($lastName, $firstName, $tel, $email, $subject, $message){
