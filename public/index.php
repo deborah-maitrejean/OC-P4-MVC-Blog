86b683fb94203319d0fae01ab53396698df46aec
deborah-maitrejean <deborah.maitrejean@gmail.com>
2018-05-06 10:59:54 +0200

    update use and include in index

--- public/index.php
+++ public/index.php
@@ -1,9 +1,10 @@
 <?php
 use \Services\Router;
+use \Autoloader\MyAutoload;
 
 require('../vendor/digitalnature/php-ref/ref.php');
 include_once('../config/_config.php');
-include_once('../MyAutoload.php');
+include_once('../autoloader/MyAutoload.php');
 MyAutoload::start();
 
 if($_GET) {
