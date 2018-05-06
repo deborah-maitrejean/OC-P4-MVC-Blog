bc88262f061f4deea3de17ac440ccf35266a1ae6
deborah-maitrejean <deborah.maitrejean@gmail.com>
2018-05-06 10:57:52 +0200

    delete comment

--- autoloader/MyAutoload.php
+++ autoloader/MyAutoload.php
@@ -6,7 +6,6 @@ class MyAutoload{
         spl_autoload_register(array(__CLASS__, 'autoload'));
     }
     public static function autoload($class){
-        //var_dump($class);die;
         $path = str_replace('\\', '/', $class);
         require '../'.$path.'.php';
     }
