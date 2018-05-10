648a94ccb1995d9f16280f3c2bbb44e28a88ce59
deborah-maitrejean <deborah.maitrejean@gmail.com>
2018-05-10 19:16:35 +0200

    create getController method with switch in Container

--- services/Container.php
+++ services/Container.php
@@ -5,5 +5,25 @@ namespace Services;
 
 class Container
 {
+    public function getController($controller)
+    {
+        $this->controller = $controller;
 
+        switch ($controller) {
+            case 'Frontend':
+                $controller = new Frontend();
+                break;
+            case 'Login':
+                $controller = new Login();
+                break;
+            case 'Backend':
+                $controller = new Backend();
+                break;
+            case 'Contact':
+                $controller = new Contact();
+                break;
+        }
+
+        return $controller;
+    }
 }
