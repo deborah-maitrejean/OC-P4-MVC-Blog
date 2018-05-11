6e398a927500e81c7b8f9eb3032b5c92a275e6f3
deborah-maitrejean <deborah.maitrejean@gmail.com>
2018-05-11 16:18:32 +0200

    update resolve method in router with condition

--- services/Router.php
+++ services/Router.php
@@ -231,31 +231,36 @@ class Router
      */
     public function resolve()
     {
-        $controller = null;
-        foreach ($this->routes as $pattern => $controllerMethod) {
-            if (preg_match($pattern, $this->uri, $matches)) {
-                $controller = $this->routes[$pattern]['controller'];
-                $method = $this->routes[$pattern]['method'];
-                $this->params = [];
-                foreach ($matches as $key => $value) {
-                    if ($key > 0) {
-                        $this->params[] = $value;
+        if (false === array_key_exists('action', $_GET)){
+            header('Location: index.php?action=home');
+            exit();
+        } else {
+            $controller = null;
+            foreach ($this->routes as $pattern => $controllerMethod) {
+                if (preg_match($pattern, $this->uri, $matches)) {
+                    $controller = $this->routes[$pattern]['controller'];
+                    $method = $this->routes[$pattern]['method'];
+                    $this->params = [];
+                    foreach ($matches as $key => $value) {
+                        if ($key > 0) {
+                            $this->params[] = $value;
+                        }
                     }
                 }
             }
-        }
-        if (!is_null($controller)) {
-            $this->controller = $controller;
-            $this->method = $method;
-            return [
-                'controller' => $this->controller,
-                'method' => $this->method,
-                'params' => $this->params,
-            ];
-        } else {
-            header('HTTP/1.0 404 Not Found');
-            include_once("../view/frontend/404.php");
-            exit();
+            if (!is_null($controller)) {
+                $this->controller = $controller;
+                $this->method = $method;
+                return [
+                    'controller' => $this->controller,
+                    'method' => $this->method,
+                    'params' => $this->params,
+                ];
+            } else {
+                header('HTTP/1.0 404 Not Found');
+                include_once("../view/frontend/404.php");
+                exit();
+            }
         }
     }
 /*
